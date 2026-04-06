8a6e98623463f29c39109cffccb9d3ed52b68564
Dalia Alquwaidhi <s202282360@kfupm.edu.sa>
2026-04-06 11:26:26 +0300

    Complete TODO 5 add Morgan middleware

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
-
-// TODO 5: Import and apply Morgan middleware
-
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
 
