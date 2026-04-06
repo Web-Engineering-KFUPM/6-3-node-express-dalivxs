3d0a63b34a1b038ea897b8f068b566a8b6bac239
Dalia Alquwaidhi <s202282360@kfupm.edu.sa>
2026-04-06 11:28:44 +0300

    Complete TODO 6.2 add quote API route

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { getRandomQuote } from "./quotes.js";
 
 const app = express();
 const PORT = 3000;
@@ -12,7 +13,9 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-// TODO 6.2: Create "/api/quote" route
-
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 // TODO 7: Start server using app.listen
